d84fd448c511433147d0e048b7f98d67b9d8c528
Ibroximov Isroiljon <isroiljonibroximov2@gmail.com>
2026-06-20 14:13:34 +0500

    3-push

---
+++ kitobbot/main.py
@@ -0,0 +1,8 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+
+from config import BOT_TOKEN
+from database.db import init_db
+from handlers import start, books, audio, tests, timer, rating, certificates, admin, tales
